cfe6a0b464cbf378d390b5161d14e83482a65767
Rafael Hengen Ribeiro <rafaelhr.ribeiro@gmail.com>
2023-10-06 21:30:15 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rafael on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
